398489dcf9331a42cf8f66423b3fcf7f4dfdc8e1
cl24abg <cl24abg@herts.ac.uk>
2025-01-06 18:03:27 +0000

    Update Study Plan count in footer to consider both studying now and later

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ function loadContent(){
 // Function to populate nav links dynamically based on selected page
 function populateNavLinks() {
     const navLinks = document.getElementById('nav-links');
-    const studyPlanCount = storageController.getStudyPlanCount();
+    const topicStudyCount = storageController.getTopicStudyCount();
 
     // Get the current page's URL
     const currentPage = window.location.pathname;
@@ -39,7 +39,7 @@ function populateNavLinks() {
         href: '/UOH-AWA/components/pages/study-plan.html',
         text: 'Study Plan',
         countClass: 'study-plan-count',
-        count: studyPlanCount,
+        count: topicStudyCount,
         classes: 'nav-link',
     };
     const homeLink = { 
